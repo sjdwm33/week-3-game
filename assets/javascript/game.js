2c06f397e1d3676139f49ca27fc199684f405799
Sam Malefyt <sdwm33@gmail.com>
2017-02-24 19:02:12 -0600

    updated game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,39 @@
-      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      var computerChoices = ["a", "e", "i", "o", "u",];
 
       var wins = 0;
       var losses = 0;
-      // Need to edit the two below*****************
       var guessCounter = 9;
-      var maxGuess = 9;
+      var gameOver = 0;
 
 
       document.onkeyup = function(event) {
 
         var userGuess = event.key.toLowerCase();
-        console.log(userGuess);
 
 
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
         if (userGuess===computerGuess) {
             wins++;
-            guessCounter--;
+            guessCounter = 9;
           }
-
-        else if (userGuess !== computerGuess) {
-          losses++;
+        else {
           guessCounter--;
         }
 
+        if(guessCounter === 0){
+          losses++;
+          guessCounter = 9;
+        }
 
-          // Here we create the HTML that will be injected into our div and displayed on the page.
-          var html = "<p>Guess what letter I'm thinking of</p>" +
+
+
+          var html = "<p>Guess what vowel I'm thinking of</p>" +
           "<p>Wins: " + wins + "</p>" +
           "<p>Losses: " + losses + "</p>" +
           "<p>Guess left:" + guessCounter + "</p>" +
           "<p>Your guesses so far:" + userGuess + "</p>";
 
-          // Injecting the HTML we just created into our div and updating the game information on our page.
           document.querySelector("#game").innerHTML = html;
 
         };
