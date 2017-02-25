a4e311365135cbd40c0ba1b72c63136bb261973e
Sam Malefyt <sdwm33@gmail.com>
2017-02-24 20:14:36 -0600

    final files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,25 @@
-      var computerChoices = ["a", "e", "i", "o", "u",];
+      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
       var wins = 0;
       var losses = 0;
       var guessCounter = 9;
-      var gameOver = 0;
+      var guessList = document.getElementById('guesses');
+      
+      guessList.innerHTML="";
 
 
       document.onkeyup = function(event) {
 
         var userGuess = event.key.toLowerCase();
-
-
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        
+        guessList.innerHTML += userGuess + ", "
 
         if (userGuess===computerGuess) {
             wins++;
             guessCounter = 9;
+            guessList.innerHTML="";
+
           }
         else {
           guessCounter--;
@@ -24,16 +28,21 @@
         if(guessCounter === 0){
           losses++;
           guessCounter = 9;
+          guessList.innerHTML="";
         }
 
 
+        var winsDiv = document.getElementById('wins');
+        var lossesDiv = document.getElementById('losses');
+        var guessLeftDiv = document.getElementById('guessLeft');
 
-          var html = "<p>Guess what vowel I'm thinking of</p>" +
-          "<p>Wins: " + wins + "</p>" +
-          "<p>Losses: " + losses + "</p>" +
-          "<p>Guess left:" + guessCounter + "</p>" +
-          "<p>Your guesses so far:" + userGuess + "</p>";
 
-          document.querySelector("#game").innerHTML = html;
+        winsDiv.innerHTML=wins;
+        lossesDiv.innerHTML=losses;
+        guessLeftDiv.innerHTML=guessCounter;
+
+
+
+
 
         };
